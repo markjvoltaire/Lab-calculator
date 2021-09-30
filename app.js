fe27b64515fd333f5e392b586a1cec056600ca2e
Mark Voltaire <markjvoltaire@gmail.com>
2021-09-30 18:26:15 -0400

    console prints addition

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+import {add} from './calculations.js';
+
 // import functions and grab DOM elements
 const nom1 = document.getElementById('num1');
 const nom2 = document.getElementById('num2');
 const addButton = document.getElementById('equalButton');
+const getAnswer = document.getElementById('answer');
 
 // initialize global state
 
 
 // set event listeners 
+addButton.addEventListener('click', ()=>{
+    const add1 = Number(nom1.value);
+    const add2 = Number(nom2.value);
+    const getAnswer = add(add1, add2);
+    console.log(getAnswer);
+  
+});
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
