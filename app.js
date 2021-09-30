e6916ef2a7720665e06eb2ef53de0028fa204034
Mark Voltaire <markjvoltaire@gmail.com>
2021-09-30 19:09:46 -0400

    subtraction buttion added and works

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-import {add} from './calculations.js';
+import { add } from './calculations.js';
+import { sub } from './calculations.js';
 
 // import functions and grab DOM elements
 const nom1 = document.getElementById('num1');
 const nom2 = document.getElementById('num2');
 const addButton = document.getElementById('equalButton');
 const getAnswer = document.getElementById('answer');
+
+const subNum1 = document.getElementById('subNum1');
+const subNum2 = document.getElementById('subNum2');
+const subButton = document.getElementById('subEqualButton');
+const getSubAnswer = document.getElementById('subAnswer');
+
 
 // initialize global state
 
@@ -16,6 +23,15 @@ addButton.addEventListener('click', ()=>{
     const result = add(add1, add2);
     
     getAnswer.textContent = result;
+});
+
+subButton.addEventListener('click', ()=>{
+    const sub1 = Number(subNum1.value);
+    const sub2 = Number(subNum2.value);
+    const subResult = sub(sub1, sub2);
+
+    getSubAnswer.textContent = subResult;
+
 });
 
 
--- calculations.js
+++ calculations.js
@@ -1,3 +1,7 @@
 export function add(num1, num2) {
     return num1 + num2;
 }
+
+export function sub(subNum1, subNum2) {
+    return subNum1 - subNum2;
+}
