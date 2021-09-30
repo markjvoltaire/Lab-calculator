3058837598d14c58d3466b06ab2dd2ad9d2ac447
Mark Voltaire <markjvoltaire@gmail.com>
2021-09-30 17:11:42 -0400

    added inputs & buttons

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // import functions and grab DOM elements
+const nom1 = document.getElementById('num1');
+const nom2 = document.getElementById('num2');
+const addButton = document.getElementById('equalButton');
 
 // initialize global state
+
 
 // set event listeners 
   // get user input
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,20 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add } from '../calculations.js';
+
+
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const num1 = 2;
+    const num2 = 3;
+    const expected = 5;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = 5;
 
     //Expect
     // Make assertions about what is expected versus the actual result
