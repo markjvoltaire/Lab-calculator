95d667c7f0e9d42ef20c9500fa109d5408a58bb7
Mark Voltaire <markjvoltaire@gmail.com>
2021-10-01 14:54:43 -0400

    Add, Sub, & Mul test passed

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,7 @@
 // IMPORT MODULES under test here:
-import { add } from '../calculations.js';
+import { add, mul } from '../calculations.js';
+import { sub } from '../calculations.js';
+
 
 
 
@@ -18,5 +20,35 @@ test('time to test a function', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('add', (expect) => {
+    const add1 = 2;
+    const add2 = 3;
+    const expected = 5;
+    const actual = add(add1, add2);
+
+    expect.equal(actual, expected);
+});
+
+
+test('sub', (expect) => {
+    const sub1 = 5;
+    const sub2 = 5;
+    const expected = 0;
+    const actual = sub(sub1, sub2);
+
+    expect.equal(actual, expected);
+});
+
+
+test('mul', (expect) => {
+    const mul1 = 5;
+    const mul2 = 5;
+    const expected = 25;
+    const actual = mul(mul1, mul2);
+
     expect.equal(actual, expected);
 });
